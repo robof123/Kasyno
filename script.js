0011498465257f505c6bd4de1e82e003371353bc
robof123 <jbhm04@gmail.com>
2024-11-04 20:58:05 +0100

    Update script.js

--- script.js
+++ script.js
@@ -81,14 +81,18 @@ function updateBalanceDisplay() {
     balanceDisplay.textContent = `Saldo: ${balance}`;
 }
 
+
 //Formularz z Hasłem
 function checkPassword() {
     const password = document.getElementById("password").value;
-    const correctPassword = "mojehaslo"; // Tutaj wpisz swoje hasło
+    const correctPassword = "mojehaslo"; // Wpisz swoje hasło
     const errorMessage = document.getElementById("error-message");
 
     if (password === correctPassword) {
-        window.location.href = "https://robof123.github.io/Kasyno/success.htmlm"; // Wpisz tutaj adres strony docelowej // Do edycji w GH
+        // Zapisz informację o zalogowaniu w Local Storage
+        localStorage.setItem("isAuthenticated", "true");
+        // Przekierowanie do strony docelowej
+        window.location.href = "success.html";
     } else {
         errorMessage.textContent = "Nieprawidłowe hasło. Spróbuj ponownie.";
     }
